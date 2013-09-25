0dd97df7975b947a707b83292354276148109c08
Rahul Horé <hore.rahul@gmail.com>
2013-09-25 10:09:03 -0400

    Cleans up quiz.rb

--- quiz.rb
+++ quiz.rb
@@ -2,12 +2,11 @@
 def square(x)
   x * x
 end
-
-# Question 2: Create a method called 'parse_true', which takes a string parameter called `questionable_string` and if the text of the string is 'true' then return true, else return false. 
+ 
+# Question 2: Create a method called 'to_bool', which takes a string parameter called `questionable_string` and if the text of the string is 'true' then return true, else return false. 
 def to_bool(questionable_string)
   questionable_string == 'true' ? true : false
 end
-# Question 2: Create a method called 'to_bool', which takes a string parameter called `questionable_string` and if the text of the string is 'true' then return true, else return false. 
 
 # Question 3. Create a method called `is_prime`, which calculates if an input parameter `x` is prime, and return true or false. Just brute force this and don't try to be too smart about it. 
 def is_prime(x)
