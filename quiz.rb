fa482966c6421f8fde0cde24dd8513c4c4a75287
Ryan Page <rpage326@gmail.com>
2013-09-25 10:52:17 -0400

    Question 2 Finished - Stuck on 3

--- quiz.rb
+++ quiz.rb
@@ -4,9 +4,21 @@ def square(x)
 end
 
 def to_bool(questionable_string)
-	questionable_string == true
-	true
-	else
-	false
+	if questionable_string == "true"
+		true
+		else
+		false
+	end
 end
 # Question 3. Create a method called `is_prime`, which calculates if an input parameter `x` is prime, and return true or false. Just brute force this and don't try to be too smart about it. 
+
+def is_prime(x)
+	return true if x == 1 
+	while i < x
+		test = x % 2
+	end
+	
+
+		
+
+end
