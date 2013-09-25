eba2364fb2376c885b42660d516e5ef9f26fadaf
Matt Clement <darkmoves@gmail.com>
2013-09-25 10:53:49 -0400

    Cleaned up quiz.

--- quiz.rb
+++ quiz.rb
@@ -1,13 +1,14 @@
 # Question 1: Create a method called 'square' which takes a parameter `x` and squares it
-# Define the square method. 
+
 def square(x)
 	x.to_i**2
 end
+
 # Call the square method on 4.
 puts square(4)
 
 # Question 2: Create a method called 'parse_true' (nope, to_bool), which takes a string parameter called `questionable_string` and if the text of the string is 'true' then return true, else return false. 
-# Define the to_bool method.
+
 def to_bool(questionable_string)
 	if questionable_string == "true"
 		return true
@@ -15,6 +16,7 @@ def to_bool(questionable_string)
 		return false
 	end
 end
+
 # Call the square method on true, then false and then 3.
 puts to_bool("true")
 puts
@@ -23,9 +25,6 @@ puts
 puts to_bool(3)
 
 # Question 3. Create a method called `is_prime`, which calculates if an input parameter `x` is prime, and return true or false. Just brute force this and don't try to be too smart about it. 
-#Define the is_prime method.
-
-#Primes: 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97.
 
 def is_prime(x)
 	i = x - 1
@@ -39,10 +38,3 @@ def is_prime(x)
 end
 
 puts is_prime(29)
-
-
-
-
-
-
-
