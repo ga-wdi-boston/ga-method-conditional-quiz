38803b5031f01b5ba02c1e7775dab82750cde589
Prescott Murphy <prurph@gmail.com>
2014-01-08 09:13:13 -0500

    Added is_prime? and named methods returning booleans with terminating ?

--- quiz.rb
+++ quiz.rb
@@ -3,15 +3,18 @@ def square(x)
   x ** 2
 end
 # Question 2: Create a method called 'to_bool', which takes a string parameter called `questionable_string` and if the text of the string is 'true' then return true, else return false.
-def to_bool(questionable_string)
+def to_bool?(questionable_string)
   unless questionable_string == "true"
     return false
   end
   true
 end
 # Question 3. Create a method called `is_prime`, which calculates if an input parameter `x` is prime, and return true or false. Just brute force this and don't try to be too smart about it.
-
-puts square(10)
-puts to_bool("truthy")
-puts to_bool("true")
-puts to_bool("false")
+def is_prime?(x)
+  (2...x).each do |num|
+    if x % num == 0
+      return false
+    end
+  end
+  return true
+end
