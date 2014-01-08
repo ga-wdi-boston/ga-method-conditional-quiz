73b284e1cf7ec14332524ff07721f327fedaff1d
Jack Henley <jack.d.henley@gmail.com>
2014-01-08 09:21:08 -0500

    All three problems done

--- quiz.rb
+++ quiz.rb
@@ -15,7 +15,16 @@ def to_bool(string)
 end
 
 # Question 3. Create a method called `is_prime`, which calculates if an input parameter `x` is prime, and return true or false. Just brute force this and don't try to be too smart about it.
-
+def is_prime(x)
+	for num in 2..(x-1)
+		if (x % num) == 0
+			return false
+		else
+			return true
+		end
+	end
+end
 
 puts square(4)
 puts to_bool('true')
+puts is_prime(3)
