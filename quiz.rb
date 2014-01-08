75b8baf67606a7a91391b53d9598c25873cf5370
emreiser <emilie.reiser@gmail.com>
2014-01-08 09:59:18 -0500

    Fixed prime function

--- quiz.rb
+++ quiz.rb
@@ -14,11 +14,14 @@ def to_bool(questionable_string)
 	end
 end
 
+def to_bool2(questionable_string)
+	questionable_string == 'true'
+end
 
 # Question 3. Create a method called `is_prime`, which calculates if an input parameter `x` is prime, and return true or false. Just brute force this and don't try to be too smart about it.
 
 def is_prime(x)
-	tests = [2..x.to_i]
+	tests = (2..(x-1)).to_a
 	tests.each do |num|
 		if x % num == 0
 			return false
