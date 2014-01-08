890b222f0ad644ee8e9ecb8ede032b62a3dc54c0
emreiser <emilie.reiser@gmail.com>
2014-01-08 09:28:46 -0500

    Added last 2 functions

--- quiz.rb
+++ quiz.rb
@@ -6,4 +6,33 @@ end
 
 # Question 2: Create a method called 'to_bool', which takes a string parameter called `questionable_string` and if the text of the string is 'true' then return true, else return false.
 
+def to_bool(questionable_string)
+	if questionable_string == 'true'
+		return true
+	else
+		return false
+	end
+end
+
+
 # Question 3. Create a method called `is_prime`, which calculates if an input parameter `x` is prime, and return true or false. Just brute force this and don't try to be too smart about it.
+
+def is_prime(x)
+	tests = [2..x]
+	tests.each |num|
+		if x % num == 0
+			return false
+		else
+			return true
+		end
+end
+
+
+#Give answer
+def give_answer(answer)
+	puts answer
+end
+
+
+
+
