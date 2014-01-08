90b8f0306005616b9701cc70a09ed35b43dbd4df
K Mak <karen.mak@stx.oxon.org>
2014-01-08 09:12:02 -0500

    question 2 answered, might need editing

--- quiz.rb
+++ quiz.rb
@@ -7,7 +7,12 @@ end
 # which takes a string parameter called `questionable_string`
 # and if the text of the string is 'true' then return true,
 # else return false.
-
+def too_bool("questionable_string")
+  return true if "#{questionable_string}" == true
+  else return false
+end
 # Question 3. Create a method called `is_prime`,
-# which calculates if an input parameter `x` is prime, and return true or false.
+# which calculates if an input parameter `x` is prime,
+# and return true or false.
 # Just brute force this and don't try to be too smart about it.
+
