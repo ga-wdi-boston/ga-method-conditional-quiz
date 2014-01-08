126df6bef68fffeb034b7168e99a0aa6056afdbe
Laure Fischer <laurefischer@Laures-MacBook-Air.local>
2014-01-08 09:22:54 -0500

    added is_prime method

--- quiz.rb
+++ quiz.rb
@@ -5,12 +5,18 @@ end
 
 # Question 2: Create a method called 'to_bool', which takes a string parameter called `questionable_string` and if the text of the string is 'true' then return true, else return false.
 def to_bool(questionable_string)
-  if questionable_string == true
+  if questionable_string == "true"
     puts true
   else
     puts false
   end
 end
 
-to_bool (hello)
 # Question 3. Create a method called `is_prime`, which calculates if an input parameter `x` is prime, and return true or false. Just brute force this and don't try to be too smart about it.
+def is_prime(x)
+  y = rand(1..1000)
+  if y != 1 & y != x
+    if x%y == 0
+      puts "false"
+
+
