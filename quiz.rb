02fe0675e3e761fe5aaa9d75e750455e5b2a97ee
Laure Fischer <laurefischer@Laures-MacBook-Air.local>
2014-01-08 09:11:38 -0500

    created to_bool method

--- quiz.rb
+++ quiz.rb
@@ -1,8 +1,16 @@
 # Question 1: Create a method called 'square' which takes a parameter `x` and squares it
-def square (x)
+def square(x)
   x * x
 end
 
 # Question 2: Create a method called 'to_bool', which takes a string parameter called `questionable_string` and if the text of the string is 'true' then return true, else return false.
+def to_bool(questionable_string)
+  if questionable_string == true
+    puts true
+  else
+    puts false
+  end
+end
 
+to_bool (hello)
 # Question 3. Create a method called `is_prime`, which calculates if an input parameter `x` is prime, and return true or false. Just brute force this and don't try to be too smart about it.
