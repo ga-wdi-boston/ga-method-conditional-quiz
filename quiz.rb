7f97fde99560fcdd9c991466619bb6ecde44776f
Praggya Rustagi Barretto <praggya@gmail.com>
2014-01-08 09:20:15 -0500

    Question 3-maybe

--- quiz.rb
+++ quiz.rb
@@ -17,3 +17,10 @@ end
 
 # Question 3. Create a method called `is_prime`, which calculates if an input parameter `x` is prime, and return true or false. Just brute force this and don't try to be too smart about it.
 
+def is_prime(x)
+	if x == prime
+		result true
+	else
+		result false
+	end
+end
